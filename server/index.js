3585bc00fdf10c8384a7f368a6bc39126760ea49
manicharan-12 <gade.manicharan12@gmaail.com>
2024-08-10 10:32:58 +0530

    Update in API

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000','https://enhanced-premium-features.vercel.app/'],
+  origin: 'https://enhanced-premium-features.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
